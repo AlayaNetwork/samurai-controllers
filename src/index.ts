f480eb4713721322fc2e50dbc5277f47adb0d539
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-29 13:31:42 +0000

    Deploy bitpshr/gaba to github.com/bitpshr/gaba.git:gh-pages

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AddressBookController';
-export * from './BaseController';
-export * from './ComposableController';
-export * from './CurrencyRateController';
-export * from './NetworkStatusController';
-export * from './TokenRatesController';
